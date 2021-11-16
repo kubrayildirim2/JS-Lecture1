e9a892a0b258111c47b79eedda7601f66af0c031
kubrayildirim2 <94444915+kubrayildirim2@users.noreply.github.com>
2021-11-16 21:53:51 +0100

    Add files via upload

---
+++ JScalisma/lecture3.js
@@ -0,0 +1,10 @@
+
+<script>
+var number1, number2, sum;
+
+number1 = prompt('please write the first number');
+number2 = prompt('please write the second number');
+sum = parseInt(number1) + parseInt(number2);
+alert('sum = '+ sum);
+
+</script>
